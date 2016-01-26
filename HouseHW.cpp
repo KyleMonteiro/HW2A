d94ec1d8b1a4a8e5e0780d2518fc3713ad91eb16
Kyle Monteiro <Kyle Monteiro>
2016-01-25 19:44:29 -0500

    First push

---
+++ HouseHW.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+class Room {
+private:
+	int width;
+	int length;
+	string name;
+public:
+	void setWidth(int w) {
+		width = w;
+	}
+	void setLength(int l) {
+		length = l;
+	}
+	void setName(string n) {
+		name = n;
+	}
+	string getName() {
+		return name;
+	}
+	int getArea() {
+		return width*length;
+	}
+};
+
+class House {
+private:
+	int roomCount = 0;
+	Room* rooms[10];
+public:
+	void addRoom(int w, int l, string n) {
+			rooms[roomCount]->setWidth(w);
+			rooms[roomCount]->setLength(l);
+			rooms[roomCount]->setName(n);
+			roomCount++;
+			cout << "Room Count is: " << roomCount << endl;
+	}
+	void printRoomNames() {
+		for (int i = 0;i < roomCount;i++) {
+				cout << rooms[i]->getName() << " ";
+		}
+		cout << endl;
+	}
+	int calculateArea() {
+		int totalArea = 0;
+		for (int i = 0; i < roomCount; i++) {
+			totalArea = totalArea + rooms[i]->getArea();
+		}
+		return totalArea;
+	}
+};
+
+int main() {
+
+	House* myHouse = new House();
+
+	ifstream RoomInfo;
+	RoomInfo.open("roomInfo.txt");
+
+	int w;
+	int l;
+	string n;
+
+	RoomInfo >> w;
+
+	while (RoomInfo) {
+		RoomInfo >> l >> n;
+		myHouse->addRoom(w, l, n);
+		RoomInfo >> w;
+	}
+
+	myHouse->printRoomNames();
+	cout << "the area of my house is " << myHouse->calculateArea()
+		<< " square feet" << endl;
+}
